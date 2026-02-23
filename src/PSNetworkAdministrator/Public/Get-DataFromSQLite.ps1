8c31a66a08dcaffda006fb9622cf5594d5dad4f9
Niklas Schneider <niklas.schneider1996@outlook.com>
2026-02-23 13:06:21 +0100

    Refactor Get-DataFromSQLite function: replace hashtable with individual parameters for indices, enhance error handling, and improve SQLite connection management.

--- src/PSNetworkAdministrator/Public/Get-DataFromSQLite.ps1
+++ src/PSNetworkAdministrator/Public/Get-DataFromSQLite.ps1
@@ -6,36 +6,60 @@ function Get-DataFromSQLite {
     [CmdletBinding()]
     param(
         [Parameter(Mandatory)]
-        [hashtable]$DataIndices,
+        [string]$DataTableName,
 
         [Parameter(Mandatory)]
-        [string]$DataTableName
+        $DataIndexName1,
+
+        $DataIndexName2,
+
+        [Parameter(Mandatory)]
+        $DataIndexValue1,
+
+        $DataIndexValue2
     )
 
     # === check parameters ===
-    $DataIndicesIsNotEmpty = Test-FunctionVariables -Param $DataIndices
     $DataTableNameIsNotEmpty = Test-FunctionVariables -Param $DataTableName
-    if (-not $DataIndicesIsNotEmpty -or -not $DataTableNameIsNotEmpty) {throw "Data indices/data table name is null/empty."}
+    $DataIndexName1IsNotEmpty = Test-FunctionVariables -Param $DataIndexName1
+    $DataIndexValue1IsNotEmpty = Test-FunctionVariables -Param $DataIndexValue1
+    if (-not $DataIndexName1IsNotEmpty -or -not $DataTableNameIsNotEmpty -or -not $DataIndexValue1IsNotEmpty) {throw "Data table name/Index name/Index value is null/empty."}
     
     # === check, if SQLite file is available ===
     # $DataFilePath = $script:DBFilePath
-    # if (-not (...)) {throw "No SQLite file found to get data from."}
+    # if (-not (...)) {throw "No SQLite file found to get data from '$DataTableName'."}
 
     # === get data from SQLite ===
     try {
-        # format the indeces as list
-        $DataIndicesList = $DataIndices -join ", "
+        $DataIndexName2IsNotEmpty = Test-FunctionVariables -Param $DataIndexName2
+        $DataIndexValue2IsNotEmpty = Test-FunctionVariables -Param $DataIndexValue2
 
-        # get schema from table
-        $DataSchemaFull = Get-SQLiteSchemaDefinition -DataTableName $DataTableName
-        $DataSchema = $DataSchemaFull.DataSchema
+        # connect index name and value
+        $SQLWhereStatement = if ($DataIndexName2IsNotEmpty -and $DataIndexValue2IsNotEmpty) {
+            "$DataIndexName1=$DataIndexValue1 AND $DataIndexName2=$DataIndexValue2"
+        }
+        else {
+            "$DataIndexName1=$DataIndexValue1"
+        }
 
-        
+        # open SQLite connection
+        $SQLiteConnection = [Microsoft.Data.Sqlite.SqliteConnection]::new("Data Source=$DataFilePath")
+        $SQLiteConnection.Open()
+
+        # write data into the table
+        $SQLiteCommandText = "SELECT * FROM $DataTableName WHERE $SQLWhereStatement;"
+        $SQLiteCommand = $SQLiteConnection.CreateCommand()
+        $SQLiteCommand.CommandText = $SQLiteCommandText
+
+        $SQLiteCommand.ExecuteNonQuery() | Out-Null
     }
     catch {
-
+        Write-AppLogging -LoggingMessage "Failed to get data from '$DataTableName': $($_.Exception.Message)" -LoggingLevel "Error"
+        throw "Failed to get data from '$DataTableName': $($_.Exception.Message)"
     }
     finally {
-
+        # close SQLite command and connection
+        if ($SQLiteCommand) {$SQLiteCommand.Dispose()}
+        if ($SQLiteConnection) {$SQLiteConnection.Dispose()}
     }
 }
