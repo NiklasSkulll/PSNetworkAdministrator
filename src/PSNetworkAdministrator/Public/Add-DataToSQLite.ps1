6b3cbd4679b58fce05ec79b82572180af1ae72f3
Niklas Schneider <niklas.schneider1996@outlook.com>
2026-04-12 18:27:01 +0200

    Refactor error handling: add new error messages for SQLite operations and enhance logging for data addition failures.

--- src/PSNetworkAdministrator/Public/Add-DataToSQLite.ps1
+++ src/PSNetworkAdministrator/Public/Add-DataToSQLite.ps1
@@ -12,31 +12,36 @@ function Add-DataToSQLite {
         [pscustomobject]$DataObject,
 
         [Parameter(Mandatory)]
-        [string]$DataTableName
+        [string]$DataTableName,
+
+        [ValidateSet('de', 'en')]
+        [string]$Language = 'en'
     )
 
     # === check if DLL loaded ===
     if (-not $script:SQLiteAvailable) {
-        Write-AppLogging -LoggingMessage "Failed to load SQLite DLL during the PSNetworkAdministrator module import." -LoggingLevel "Error"
-        throw "Failed to load SQLite DLL during the PSNetworkAdministrator module import."
+        $ErrorMessage = Get-ErrorMessages -ErrorCode 'INx0000004' -DomainName $DomainName -Language $Language
+        
+        Write-AppLogging -LoggingMessage $ErrorMessage -LoggingLevel 'Error' -Language $Language
+        throw $ErrorMessage
     }
 
     # === initialize data schema ===
     try {
-        $InitializedDataSchema = Initialize-SQLiteSchema -DomainName $DomainName -DataTableName $DataTableName -DataObject $DataObject
+        $InitializedDataSchema = Initialize-SQLiteSchema -DomainName $DomainName -DataTableName $DataTableName -DataObject $DataObject -Language $Language
     }
     catch {
-        throw "Failed to initialize schema: $($_.Exception.Message)"
+        throw "$($_.Exception.Message)"
     }
     
     # === SQLite file path ===
     $DataFilePath = $script:DBFilePath
-    Initialize-FilePath -FilePath $DataFilePath 
+    Initialize-FilePath -FilePath $DataFilePath -Language $Language
 
     # === create table and write data ===
     try {
         # create the SQLite table if it doesn't exists
-        Initialize-SQLiteTable -DomainName $DomainName -DataTableName $DataTableName -DataFilePath $DataFilePath -DataObject $DataObject
+        Initialize-SQLiteTable -DomainName $DomainName -DataTableName $DataTableName -DataFilePath $DataFilePath -DataObject $DataObject -Language $Language
 
         # open SQLite connection
         $SQLiteConnection = [Microsoft.Data.Sqlite.SqliteConnection]::new("Data Source=$DataFilePath")
@@ -66,11 +71,15 @@ function Add-DataToSQLite {
         }
         $SQLiteCommand.ExecuteNonQuery() | Out-Null
 
-        Write-AppLogging -LoggingMessage "Successfully saved data into SQLite table: $DataTableName" -LoggingLevel "Info"
+        $InfoMessageRefVar = '$DataTableName'
+        $InfoMessage = if ($Language -eq "de") {'Daten wurden erfolgreich in SQLite-Tabelle gespeichert'} else {'Successfully saved data into SQLite table'}
+        Write-AppLogging -LoggingMessage "|$DomainName|$InfoMessageRefVar|$DataTableName| $InfoMessage." -LoggingLevel 'Info' -Language $Language
     }
     catch {
-        Write-AppLogging -LoggingMessage "Failed to create '$DataTableName' and write data: $($_.Exception.Message)" -LoggingLevel "Error"
-        throw "Failed to create '$DataTableName' and write data: $($_.Exception.Message)"
+        $ErrorMessage = Get-ErrorMessages -ErrorCode 'DBx0000014' -ExceptionMessage "$($_.Exception.Message)" -DomainName $DomainName -VariableName '$DataTableName' -VariableValue $DataTableName -Language $Language
+        
+        Write-AppLogging -LoggingMessage $ErrorMessage -LoggingLevel 'Error' -Language $Language
+        throw $ErrorMessage
     }
     finally {
         # close SQLite command and connection
